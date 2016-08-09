54f487cfa7f53f3301a8f8938d2e1b5e837522a1
FUNABARA Masao <masao@masoo.jp>
2016-08-09 13:56:12 +0900

    fix generator domain services.

--- lib/generators/domains/domains_generator.rb
+++ lib/generators/domains/domains_generator.rb
@@ -4,12 +4,22 @@ class DomainsGenerator < Rails::Generators::NamedBase
   def create_initializer_file
     empty_directory "app/domains/#{file_name}"
     create_file "app/domains/#{file_name}/.keep"
-    inject_into_file 'config/application.rb', :before => "  end\nend" do
-    <<-"RUBY"
+
+    if File.exists? "config/application.rb"
+      inject_into_file 'config/application.rb', :before => "  end\nend" do
+        <<-"RUBY"
 
     # Configure load paths for DDD domain #{file_name}
     config.autoload_paths += %W(\#{config.root}/app/domains/#{file_name})
-    RUBY
+        RUBY
+      end
+    elsif File.exists? "lib/#{namespace.to_s.underscore}/engine.rb"
+      inject_into_file "lib/#{namespace.to_s.underscore}/engine.rb", :before => "  end\nend" do
+        <<-"RUBY"
+
+    config.autoload_paths += %W(\#{config.root}/app/domains/#{file_name})
+        RUBY
+      end
     end
   end
 end
