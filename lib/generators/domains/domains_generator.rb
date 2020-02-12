c04f3291f9308a279e7d57d0691d754c8c8b67d9
FUNABARA Masao <masao@masoo.jp>
2020-02-12 17:01:47 +0900

    setting rails 6.x.

--- lib/generators/domains/domains_generator.rb
+++ lib/generators/domains/domains_generator.rb
@@ -4,21 +4,5 @@ class DomainsGenerator < Rails::Generators::NamedBase
   def create_initializer_file
     empty_directory "app/domains/#{file_name}"
     create_file "app/domains/#{file_name}/.keep"
-
-    if File.exists? "config/initializers/ddd_domain.rb"
-      inject_into_file 'config/initializers/ddd_domain.rb', :before => "end" do
-        <<-"RUBY"
-  config.paths.add 'app/domains/#{file_name}', eager_load: true
-        RUBY
-      end
-    else
-      create_file 'config/initializers/ddd_domain.rb' do
-        <<~"RUBY"
-          Rails.configuration do |config|
-            config.paths.add 'app/domains/#{file_name}', eager_load: true
-          end
-        RUBY
-      end
-    end
   end
 end
